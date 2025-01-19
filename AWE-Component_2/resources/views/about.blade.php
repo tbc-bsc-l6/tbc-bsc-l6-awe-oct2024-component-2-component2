91c7d2e0a9d0ba048dba3883922476d0f9c8dfcb
mjenny <mjenny21@tbc.edu.np>
2025-01-19 23:47:01 +0545

    added landing page, layout and logo

---
+++ AWE-Component_2/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+<x-slot:heading>
+        this is the about page.
+    </x-slot:heading>
+    <h1>about</h1>
+</x-layout>
---
+++ AWE-Component_2/resources/views/index.blade.php
@@ -0,0 +1,6 @@
+ <x-layout>
+    <x-slot:heading>
+        this is the home page.
+    </x-slot:heading>
+    <h1>index</h1>
+</x-layout>
