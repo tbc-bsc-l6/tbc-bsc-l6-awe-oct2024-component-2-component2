c92675378787c1fc74f92c1fde698e86aced2a21
mjenny <mjenny21@tbc.edu.np>
2025-01-20 10:33:36 +0545

    removed layout page and added login page

--- AWE-Component_2/routes/web.php
+++ AWE-Component_2/routes/web.php
@@ -2,9 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
     return view('index');
 });
 Route::get('/about', function () {
     return view('about');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
